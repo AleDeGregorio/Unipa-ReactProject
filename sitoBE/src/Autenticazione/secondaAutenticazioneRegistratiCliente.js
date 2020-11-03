d4069c0d21ecf6a48e8927e3d8a3f338fd179b77
madchinese <madchinese@users.noreply.github.com>
2020-11-03 20:44:07 +0100

    BE on registrazione

--- sitoBE/src/Autenticazione/secondaAutenticazioneRegistratiCliente.js
+++ sitoBE/src/Autenticazione/secondaAutenticazioneRegistratiCliente.js
@@ -3,37 +3,81 @@ import {Form, Col, Row, Button} from 'react-bootstrap'
 import {Link} from 'react-router-dom'
 import './secondaAutenticazione.css'
 
-function SecondaAutenticazioneRegistratiCliente(){
+class SecondaAutenticazioneRegistratiCliente extends React.Component{
+  constructor(){
+      super();
+      this.state={
+          email_cl:'',
+          password_cl:'',
+          nome_cl:'',
+          cognome_cl:'',
+          data_nascita_cl:'',
+          telefono_cl:'',
+          apiResponse: [],
+          error:false,
+          errorMessage:''
+      };
+  }
+  onChange=(e)=>{
+      this.setState({[e.target.name]: e.target.value});
+  }
+  onSubmitInsert = (e) => {
+      e.preventDefault();
+      const data={
+          email_cl: this.state.email_cl,
+          password_cl: this.state.password_cl,
+          nome_cl:this.state.nome_cl,
+          cognome_cl:this.state.cognome_cl,
+          data_nascita_cl:this.state.data_nascita_cl,
+          telefono_cl:this.state.telefono_cl
+      }
+      fetch('http://localhost:9000/insertCliente/new',{
+          method:"POST",
+          headers:{
+              'Content-type' : 'application/json'
+          },
+          body: JSON.stringify(data)
+      })
+      .then((result) => result.text())
+      .then((result)=>{
+          this.setState({apiResponse:JSON.parse(result) });
+          if(this.state.apiResponse.status === 'error') {
+            this.setState({ error: true });
+            this.setState({ errorMessage: this.state.apiResponse.message });
+          }
+      })
+    }
+    render(){
     return(
-        <Form className="contenitoreAutenticazione">
+        <Form className="contenitoreAutenticazione" onSubmit={onSubmitInsert}>
             <div className="contentNewCheckAutenticazione">
                 <h2>Iscriviti</h2>
         <Form.Group as={Col} controlId="formGridName">
                     <Form.Label>Nome</Form.Label>
-                    <Form.Control type="name" placeholder="Nome" required />
+                    <Form.Control type="name" placeholder="Nome" onChange={this.props.onChange} required />
                 </Form.Group>
 
                 <Form.Group as={Col} controlId="formGridSurname">
                 <Form.Label>Cognome</Form.Label>
-                    <Form.Control type="surname" placeholder="Cognome" required/>
+                    <Form.Control type="surname" placeholder="Cognome" onChange={this.props.onChange} required/>
                 </Form.Group>
             <Form.Group as={Col} controlId="formGridEmail">
             <Form.Label>E-mail</Form.Label>
-                    <Form.Control type="email" placeholder="E-mail" required />
+                    <Form.Control type="email" placeholder="E-mail" onChange={this.props.onChange} required />
                 </Form.Group>
 
                 <Form.Group as={Col} controlId="formGridPassword">
                 <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" required/>
+                    <Form.Control type="password" placeholder="Password" onChange={this.props.onChange} required/>
                 </Form.Group>
             <Form.Group as={Col} controlId="formGridIndirizzo">
             <Form.Label>Telefono</Form.Label>
-            <Form.Control type="tel" placeholder="Telefono" required/>
+            <Form.Control type="tel" placeholder="Telefono" onChange={this.props.onChange} required/>
             </Form.Group>
 
             <Form.Group as={Col} controlId="formGridCap">
             <Form.Label>Data di nascita</Form.Label>
-            <Form.Control type="date" required className="inputSignUp"/>
+            <Form.Control type="date" required className="inputSignUp" onChange={this.props.onChange} />
             </Form.Group>
             <Link to="/secondaAutenticazioneRegistrati">Torna indietro</Link>
           </div>
@@ -42,6 +86,6 @@ function SecondaAutenticazioneRegistratiCliente(){
                     Registrati
             </Button>
         </Form>
-    );
+    );}
 }
 export default SecondaAutenticazioneRegistratiCliente
--- sitoBE/src/Autenticazione/secondaAutenticazioneRegistratiProprietario.js
+++ sitoBE/src/Autenticazione/secondaAutenticazioneRegistratiProprietario.js
@@ -3,42 +3,86 @@ import {Form, Col, Row, Button} from 'react-bootstrap'
 import {Link} from 'react-router-dom'
 import './secondaAutenticazione.css'
 
-function SecondaAutenticazioneRegistratiProprietario(){
+class SecondaAutenticazioneRegistratiProprietario extends React.Component {
+    constructor(){
+        super();
+        this.state={
+            email_prop:'',
+            password_prop:'',
+            nome_prop:'',
+            cognome_prop:'',
+            data_nascita_prop:'',
+            telefono_prop:'',
+            apiResponse: [],
+            error:false,
+            errorMessage:''
+        };
+    }
+    onChange=(e)=>{
+        this.setState({[e.target.name]: e.target.value});
+    }
+    onSubmitInsert = (e) => {
+        e.preventDefault();
+        const data={
+            email_prop: this.state.email_prop,
+            password_prop: this.state.password_prop,
+            nome_prop:this.state.nome_prop,
+            cognome_prop:this.state.cognome_prop,
+            data_nascita_prop:this.state.data_nascita_prop,
+            telefono_prop:this.state.telefono_prop
+        }
+        fetch('http://localhost:9000/insertCliente/new',{
+            method:"POST",
+            headers:{
+                'Content-type' : 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then((result) => result.text())
+        .then((result)=>{
+            this.setState({apiResponse:JSON.parse(result) });
+            if(this.state.apiResponse.status === 'error') {
+              this.setState({ error: true });
+              this.setState({ errorMessage: this.state.apiResponse.message });
+            }
+        })
+      } //manca num documento ed eventualmente foto e tipologia
+    render(){
     return(
-        <Form className="contenitoreAutenticazione">
+        <Form className="contenitoreAutenticazione" onSubmit={onSubmitInsert}>
             <div className="contentNewCheckAutenticazione">
                 <h2>Iscriviti</h2>
         <Form.Group as={Col} controlId="formGridName">
                     <Form.Label>Nome</Form.Label>
-                    <Form.Control type="name" placeholder="Nome" required />
+                    <Form.Control type="name" placeholder="Nome" onChange={this.props.onChange} required />
                 </Form.Group>
 
                 <Form.Group as={Col} controlId="formGridSurname">
                 <Form.Label>Cognome</Form.Label>
-                    <Form.Control type="surname" placeholder="Cognome" required/>
+                    <Form.Control type="surname" placeholder="Cognome" onChange={this.props.onChange} required/>
                 </Form.Group>
             <Form.Group as={Col} controlId="formGridEmail">
             <Form.Label>E-mail</Form.Label>
-                    <Form.Control type="email" placeholder="E-mail" required />
+                    <Form.Control type="email" placeholder="E-mail" onChange={this.props.onChange} required />
                 </Form.Group>
 
                 <Form.Group as={Col} controlId="formGridPassword">
                 <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" required/>
+                    <Form.Control type="password" placeholder="Password" onChange={this.props.onChange} required/>
                 </Form.Group>
             <Form.Group as={Col} controlId="formGridIndirizzo">
             <Form.Label>Telefono</Form.Label>
-            <Form.Control type="tel" placeholder="Telefono" required/>
+            <Form.Control type="tel" placeholder="Telefono" onChange={this.props.onChange} required/>
             </Form.Group>
 
             <Form.Group as={Col} controlId="formGridIndirizzo">
             <Form.Label>Documento</Form.Label>
-            <Form.Control type="tel" placeholder="Documento" required/>
+            <Form.Control type="tel" placeholder="Documento" onChange={this.props.onChange} required/>
             </Form.Group>
 
             <Form.Group as={Col} controlId="formGridCap">
             <Form.Label>Data di nascita</Form.Label>
-            <Form.Control type="date" required className="inputSignUp"/>
+            <Form.Control type="date" required className="inputSignUp" onChange={this.props.onChange} />
             </Form.Group>
             <Link to="/secondaAutenticazioneRegistrati">Torna indietro</Link>
           </div>
@@ -47,6 +91,6 @@ function SecondaAutenticazioneRegistratiProprietario(){
                     Registrati
             </Button>
         </Form>
-    );
+    );}
 }
 export default SecondaAutenticazioneRegistratiProprietario
