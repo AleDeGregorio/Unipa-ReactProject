4b9ae46d9f4eeeb0d871ac7bb0a86ba0d4af64df
AleDeGregorio <alessio.degregorio.98@gmail.com>
2020-11-11 12:27:43 +0100

    Aggiunta app

---
+++ App/App.js
@@ -0,0 +1,59 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview'
+import { BackHandler } from "react-native";
+
+export default class App extends React.Component {
+
+  WEBVIEW_REF = React.createRef();
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      canGoBack: false,
+    };
+  }
+
+  componentDidMount() {
+    BackHandler.addEventListener("hardwareBackPress", this.handleBackButton);
+  }
+
+  componentWillUnmount() {
+    BackHandler.removeEventListener("hardwareBackPress", this.handleBackButton);
+  }
+
+  handleBackButton = () => {
+    if (this.state.canGoBack) {
+      this.WEBVIEW_REF.current.goBack();
+      return true;
+    }
+  };
+
+  onNavigationStateChange = (navState) => {
+    this.setState({
+      canGoBack: navState.canGoBack,
+    });
+  };
+
+  render() {
+    return (
+      <WebView 
+        source = {{ uri: 'http://192.168.1.106:9000' }}
+        style = {{ marginTop: '10%' }}
+        ref={this.WEBVIEW_REF}
+        onNavigationStateChange={this.onNavigationStateChange}
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
