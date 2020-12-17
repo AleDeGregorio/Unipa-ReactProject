93d9fc8a4675a29a45ec9515d239b9b76771b636
madchinese <madchinese@users.noreply.github.com>
2020-12-17 19:01:47 +0100

    fine

--- Frontend/src/GestioneProprietario/ProprietarioPage.js
+++ Frontend/src/GestioneProprietario/ProprietarioPage.js
@@ -2,8 +2,13 @@ import React from 'react';
 import {Card, Modal, Button} from 'react-bootstrap'
 //import {Nav, CardColumns} from 'react-bootstrap'
 import './ProprietarioPage.css'
-import {SiCashapp} from "react-icons/si";
-import {RiAccountBoxLine} from "react-icons/ri";
+import {SiCashapp} from "react-icons/si"
+import {RiAccountBoxLine} from "react-icons/ri"
+import {AiFillCheckSquare} from "react-icons/ai"
+import {FaHotel} from "react-icons/fa"
+import {RiMailSendLine} from "react-icons/ri"
+import {FaClipboardList} from "react-icons/fa"
+import {BsTools} from "react-icons/bs"
 import {Link} from 'react-router-dom'
 
 import { Redirect } from "react-router-dom";
@@ -61,7 +66,7 @@ class ProprietarioPage extends React.Component {
     }
 
     render() {
-        if(!localStorage.getItem('logged') || !localStorage.getItem('proprietario')) {
+        /*if(!localStorage.getItem('logged') || !localStorage.getItem('proprietario')) {
             return <Redirect
                 to={{
                     pathname: "/ErrorPage",
@@ -72,72 +77,66 @@ class ProprietarioPage extends React.Component {
                 }}
             />
         }
-        else {
+        else*/ {
             var data_invio = this.state.apiResponse[0] ? new Date(this.state.apiResponse[0].ultimo_invio_dati).toLocaleDateString() : "Mai inviati";
             return(
-                <div className="carte_prop" >
-                <Link to="/Accettazione" className="LinK">
-                <Card className="prop">
-                    <Card.Title>Accetta Prenotazioni</Card.Title>
-                    <Card.Text>Visualizza le prenotazioni in attesa di essere accettate.</Card.Text>
-                </Card>
-                </Link>
-                <Link to = "/Checkin" className="LinK">
-                    <Card className="prop">
-                    <Card.Img src="https://cdn.onlinewebfonts.com/svg/img_572848.png" className="image"></Card.Img>
-                    <Card.Body>
-                    <Card.Title>Check-in</Card.Title>
-                    <Card.Text>Effettua il check-in dei soggiornanti e invia i dati all'ufficio della Questura.</Card.Text>
-                    </Card.Body>
-                   </Card>
-                </Link>
-                <Card className="prop" onClick = {this.handleShow}>
-                    <Card.Title>Invio dati Turismo </Card.Title>
-                    <Card.Text>Effettua l'invio dei dati relativi ai soggiornanti all'ufficio del Turismo.</Card.Text>
-                </Card>
-                <Modal show={this.state.show} onHide={this.handleClose}>
-                    <Modal.Header closeButton>
-                        <Modal.Title>Invio dati all'ufficio del turismo</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                       <div className="turismocont">
-                           <p>Ultimo invio dei dati all'ufficio competente: {data_invio} </p>
-                           <p>Desideri inviare i dati nuovamente?</p>
-                       </div>
-                    </Modal.Body>
-                    <Modal.Footer>
-                     <Button variant="secondary" /*funzione invia dationClick={}*/>Invia dati</Button>
-                     <Button variant="secondary" onClick={this.handleClose}>
-                      Chiudi
-                    </Button>                    
-                   </Modal.Footer>
-                </Modal>
-                
-                <Link to ="/InserimentoProprietà" className="LinK">
-                <Card className="prop">
-                    <Card.Img src="https://cdn3.iconfinder.com/data/icons/hotels-b-b-and-cabins-1/50/Hotels_BB_and_Cabins_Outline-74-512.png" className="image"></Card.Img>
-                    <Card.Title>Inserisci Proprietà</Card.Title>
-                    <Card.Text>Scegli tra le tipologie di strutture presenti nel sistema e inserisci la tua! </Card.Text>
-                </Card>
-                </Link>
-                <Link to="/SceltaModifica" className="LinK">
-                <Card  className="prop">
-                    <Card.Title>Modifica Proprietà</Card.Title>
-                    <Card.Text>Scegli tra le strutture possedute ed effettua i cambiamenti che preferisci.</Card.Text>
-                </Card></Link>
-                <Card className="prop">
-                    <RiAccountBoxLine className="image"></RiAccountBoxLine>
-                    <Card.Title>Gestione Account</Card.Title>
-                    <Card.Text>Vedi le opzioni disponibili</Card.Text>
-                </Card>
-                <Link to="/Earning" className="LinK">
-                <Card className="prop">
-                  <SiCashapp className="image"></SiCashapp>
-                    <Card.Title>Resoconto Guadagni</Card.Title>
-                    <Card.Text>Visualizza </Card.Text>
-                </Card>
-                </Link>
-                </div>
+                <div className="col">
+        <div className="carte_prop" >
+        <Link to = "" className="LinK">
+        <Card className="prop">
+            <FaClipboardList className="imageProp"/>
+            <Card.Title>Accetta Prenotazioni</Card.Title>
+            <Card.Text>Visualizza le prenotazioni in attesa di essere accettate.</Card.Text>
+        </Card>
+        </Link>
+        <Link to = "/Checkin" className="LinK">
+            <Card className="prop">
+            <AiFillCheckSquare className="imageProp"/>
+            <Card.Title>Check-in</Card.Title>
+            <Card.Text>Effettua il check-in dei soggiornanti e invia i dati all'ufficio della Questura.</Card.Text>
+           </Card>
+        </Link>
+        <Link to = "" className="LinK">
+        <Card className="prop">
+            <RiMailSendLine className="imageProp"/>
+            <Card.Title>Invio dati Turismo </Card.Title>
+            <Card.Text>Effettua l'invio dei dati relativi ai soggiornanti all'ufficio del Turismo.</Card.Text>
+        </Card>
+        </Link>
+        </div>
+        <div className="carte_prop">
+        <Link to = "/InserimentoProprietà" className="LinK">
+        <Card className="prop">
+            <FaHotel className="imageProp"/>
+            <Card.Title>Inserisci Proprietà</Card.Title>
+            <Card.Text>Scegli tra le tipologie di strutture presenti nel sistema e inserisci la tua! </Card.Text>
+        </Card>
+        </Link>
+        <Link to = "/SceltaModifica" className="LinK">
+        <Card  className="prop">
+            <BsTools className="imageProp"/>
+            <Card.Title>Modifica Proprietà</Card.Title>
+            <Card.Text>Scegli tra le strutture possedute ed effettua i cambiamenti che preferisci.</Card.Text>
+        </Card>
+        </Link>
+        <Link to = "" className="LinK">
+        <Card className="prop">
+            <RiAccountBoxLine className="imageProp"></RiAccountBoxLine>
+            <Card.Title>Gestione Account</Card.Title>
+            <Card.Text>Vedi le opzioni disponibili</Card.Text>
+        </Card>
+        </Link>
+        </div>
+        <div className="carte_prop">
+        <Link to = "/Earning" className="LinK">
+        <Card className="prop">
+          <SiCashapp className="imageProp"></SiCashapp>
+            <Card.Title>Resoconto Guadagni</Card.Title>
+            <Card.Text>Visualizza </Card.Text>
+        </Card>
+        </Link>
+        </div>
+        </div>
             );
         }
     }
